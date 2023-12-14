09c41ab5297f37dfce7e80cbe9b538978641a443
Moshe Malka <hello@moshemalka.com>
2023-12-14 08:50:24 -0500

    rumdash.io beta

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,21 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+        <script type="text/javascript">
+          function __rdOnLoad() {
+            RumDash.init({
+              key: "ua4cxM67PwNfhjiaYgHM7D",
+              env: "prod",
+              /*
+              ======================================================================
+              Optionally specify 'revision' to track releases' effect on performance
+              ======================================================================
+              revision: "[RELEASE_VERSION_OR_COMMIT_SHA]",
+              */
+            })
+          }
+        </script>
+        <script defer type="text/javascript" src="https://cdn.rumdash.io/client.js"></script>
       <body>
         <Main />
         <NextScript />
