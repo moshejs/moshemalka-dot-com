551d1232b415f6d1e740853578c7b5a27d86b477
Moshe Malka <hello@moshemalka.com>
2023-12-15 09:44:57 -0500

    RumDash beta integration

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,28 +1,27 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-        <script type="text/javascript">
-          function __rdOnLoad() {
-            RumDash.init({
-              key: "ua4cxM67PwNfhjiaYgHM7D",
-              env: "prod",
-              /*
-              ======================================================================
-              Optionally specify 'revision' to track releases' effect on performance
-              ======================================================================
-              revision: "[RELEASE_VERSION_OR_COMMIT_SHA]",
-              */
-            })
-          }
-        </script>
-        <script defer type="text/javascript" src="https://cdn.rumdash.io/client.js"></script>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
+    <Head>
+      <Script 
+        src="https://cdn.rumdash.io/client.js" 
+        strategy="afterInteractive" 
+        onLoad={() => {
+          RumDash.init({
+            key: "ua4cxM67PwNfhjiaYgHM7D",
+            env: "prod",
+            // Optionally specify 'revision' to track releases' effect on performance
+            // revision: "[RELEASE_VERSION_OR_COMMIT_SHA]",
+          });
+        }}
+      />
+    </Head>
+    <body>
+      <Main />
+      <NextScript />
+    </body>
+  </Html>
   )
 }
